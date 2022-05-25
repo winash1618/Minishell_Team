e862c880892c30342364293da9a4822b74a1cf3a
Muhammed asharaf Karuvante valappil <mkaruvan@lab3r3s10.42abudhabi.ae>
2022-05-25 09:01:46 +0400

    header file changes: added list

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*   By: mkaruvan <mkaruvan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 09:16:46 by ayassin           #+#    #+#             */
-/*   Updated: 2022/05/24 09:18:57 by ayassin          ###   ########.fr       */
+/*   Updated: 2022/05/24 09:44:11 by mkaruvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,32 @@
 
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
+
+// for saving local variable
+typedef struct var 
+{
+	char *key;
+	char *value;
+	struct var *next;
+	struct var *prev;
+}	t_var;
+
+// for saving commands
+typedef struct list
+{
+	char	*token;
+	int	flag;
+	char **var;
+	char **temp;
+	int dollar_flag;
+	struct list	*next;
+	struct list	*prev;
+}	t_new;
+
+typedef struct info
+{
+	int flag;
+	int w_flag;
+} t_info;
+
 #endif
