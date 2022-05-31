a07796dd796c3042b04cd625c7792ef423672db0
Muhammed asharaf Karuvante valappil <mkaruvan@lab3r3s10.42abudhabi.ae>
2022-05-31 05:56:46 +0400

    Implemented more conditions in dollar expansion

--- parsing/p_dollar.c
+++ parsing/p_dollar.c
@@ -6,7 +6,7 @@
 /*   By: mkaruvan <mkaruvan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:36:54 by mkaruvan          #+#    #+#             */
-/*   Updated: 2022/05/30 09:34:00 by mkaruvan         ###   ########.fr       */
+/*   Updated: 2022/05/30 17:39:32 by mkaruvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,13 @@ void dollar_expansion(t_new *cmd, char **env)
 {
 	while (cmd != NULL)
 	{
-		if (cmd->d_flag)
-		{
+		// if (cmd->d_flag)
+		// {
 			if (cmd->flag == 1)
 				cmd->es = get_expanded_string(cmd->token, env);
 			else if (cmd->flag == 3)
 				cmd->lst = get_expanded_list(cmd->token, env);
-		}
+		// }
 		cmd = cmd->next;
 	}
 }
