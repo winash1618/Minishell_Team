a8355ee09ca54f3771c50c78ba6039f1da5fa806
Ahmed Yassin <ayassin@lab1r2s10.42abudhabi.ae>
2022-07-26 18:17:42 +0400

    Excution is complete

--- execute_src/signals.c
+++ execute_src/signals.c
@@ -6,7 +6,7 @@
 /*   By: ayassin <ayassin@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 09:00:21 by ayassin           #+#    #+#             */
-/*   Updated: 2022/07/25 18:45:30 by ayassin          ###   ########.fr       */
+/*   Updated: 2022/07/26 18:12:47 by ayassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ static void	signal_handler(int signum, siginfo_t *info, void *ptr)
 	{
 		if (g_m->content != NULL)
 			cleanexit(NULL, NULL, 5, g_m->content);
-		ft_putstr_fd(">Enter a string:   \b\b\n", 2);
+		rl_redisplay();
 		rl_replace_line("", 0);
+		ft_putstr_fd("\n", 2);
 		rl_on_new_line();
 		rl_redisplay();
 	}
@@ -42,3 +43,6 @@ int	signals(void)
 	sigaction(SIGQUIT, &sa, NULL);
 	return (0);
 }
+
+// ft_putstr_fd(">Enter a string:   \b\b\n", 2); for valgrind
+// //ft_putstr_fd("\33[2K\r>Enter a string: \n", 2); for cool code
